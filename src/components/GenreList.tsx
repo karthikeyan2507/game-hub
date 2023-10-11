898cce6980b883ea7f056f826477e6b548453cc4
Karthikeyan <karthik.77email@gmail.com>
2023-10-11 15:20:37 +0530

    Filter Games by Genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,8 +1,12 @@
-import { HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
+import { Button, HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
 import useGenres, { Genre } from "../hooks/useGenre";
 import getCroppedImageUrl from "../services/Image-url";
 
-const GenreList = () => {
+interface Props{
+  onSelectGenre: (genre: Genre) => void;
+}
+
+const GenreList = ({onSelectGenre} : Props) => {
   const { Data ,isLoading , error  } = useGenres();
 
   if (error) return null;
@@ -16,7 +20,7 @@ const GenreList = () => {
           {" "}
           <HStack>
             <Image boxSize='32px' borderRadius={8} src={getCroppedImageUrl (genre.image_background) } />
-            <Text fontSize='lg' >{genre.name}</Text>
+            <Button onClick={() => onSelectGenre(genre) } fontSize='lg' variant='link' >{genre.name}</Button>
           </HStack>
         </ListItem>
       ))}
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,20 +1,21 @@
-
 import useData from "./useData";
+import { Genre } from "./useGenre";
 
-
-export interface Platform{
-    id: number;
-    name: string;
-    slug: string;
-
+export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
 }
 export interface Game {
-    id: number;
-    name: string;
-    background_image: string;
-    parent_platforms: {platform : Platform} [];
-    metacritic: number ;
+  id: number;
+  name: string;
+  background_image: string;
+  parent_platforms: { platform: Platform }[];
+  metacritic: number;
 }
 
-const useGames = () => useData<Game>('/games'); 
-export default useGames;  
+const useGames = (selectedGenre: Genre | null) =>
+  useData<Game>("/games", { params: { genres: selectedGenre?.id } }, [
+    selectedGenre?.id,
+  ]);
+export default useGames;
